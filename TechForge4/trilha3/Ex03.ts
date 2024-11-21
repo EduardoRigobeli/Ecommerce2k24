c5351e57258e31df5f2a782efe21d51a711100a4
EduardoRigobeli <eduardo.souza@grupointegrado.br>
2024-11-21 16:32:12 -0300

    Ex 03 Trilha 3 - TechForge

--- TechForge4/trilha3/Ex03.ts
+++ TechForge4/trilha3/Ex03.ts
@@ -1,1 +1,59 @@
 /*Crie uma classe Pagamento com um método processar(). Crie subclasses PagamentoCartao e PagamentoBoleto, cada uma com sua própria implementação do método processar(). A classe PagamentoCartao deve validar o número do cartão e processar o pagamento, e PagamentoBoleto deve gerar um código de boleto. Crie uma função que aceite diferentes tipos de pagamento e processe-os usando polimorfismo.*/
+
+class Pagamento {
+    public processar(): void {}
+}
+
+class PagamentoCartao extends Pagamento {
+    private numeroCartao: string;
+
+    constructor(numeroCartao:string) {
+        super()
+        this.numeroCartao = numeroCartao;
+    }
+
+    private validarCartao(): boolean {//Validar o Cartão se tem 16 dígitos
+        return this.numeroCartao.length === 16 && !isNaN(Number(this.numeroCartao))
+    }
+
+    processar() {
+        if (this.validarCartao()) {
+            console.log(`Número do Cartão válido! N° Cartão: ${this.numeroCartao}`)
+        } else {
+            console.log(`Número do cartão INVÁLIDO!`)
+        }
+    }
+}
+
+class PagamentoBoleto extends Pagamento {
+    private valor: number;
+
+    constructor(valor:number) {
+        super()
+        this.valor = valor;
+    }
+
+    private gerarCodigoBoleto(): string {
+        let codigo = '';
+        for(let i=0; i < 10; i++) {
+            codigo += Math.floor(Math.random() * 10).toString();
+        }
+        return codigo;
+    }
+
+    public processar(): void {
+        const codigoBoleto = this.gerarCodigoBoleto();
+        console.log(`Código boleto: ${codigoBoleto}. Valor: R$${this.valor.toFixed(2)}.`);
+    }
+}
+
+function processarPagamento(pagamento: Pagamento) {//Função para processar outros pagamentos
+    pagamento.processar();
+}
+
+const pagamentoCartao = new PagamentoCartao("6147983568749125");
+processarPagamento(pagamentoCartao); 
+
+
+const pagamentoBoleto = new PagamentoBoleto(150.75);
+processarPagamento(pagamentoBoleto);
